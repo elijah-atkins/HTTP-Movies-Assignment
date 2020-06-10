c99008ed203d268e9a07188782121ffb9824f760
elijah-atkins <elijahorama@gmail.com>
2020-06-09 18:17:46 -0600

    delete functional

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
-
+  const { push } = useHistory();
   const fetchMovie = (id) => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then((res) => setMovie(res.data))
       .catch((err) => console.log(err.response));
   };
+  const deleteMovie = e => {
 
+      e.preventDefault();
+      axios
+        .delete(`http://localhost:5000/api/movies/${params.id}`)
+        .then(res => {
+        push(`/`)})
+        .catch(err =>
+          console.error("Movie.js: handleDelete: err: ", err.message, err.response)
+        );
+    
+  }
   const saveMovie = () => {
     addToSavedList(movie);
   };
@@ -32,6 +43,9 @@ function Movie({ addToSavedList }) {
 
       <div className="save-button" onClick={saveMovie}>
         Save
+      </div>
+      <div className="delete-button" onClick={deleteMovie}>
+        Delete
       </div>
     </div>
   );
