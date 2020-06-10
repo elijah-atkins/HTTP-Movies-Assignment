a9a86235516086796e200950633ae03075a4d237
elijah-atkins <elijahorama@gmail.com>
2020-06-09 18:57:11 -0600

    mvp done working on styling

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,7 @@ const App = () => {
       </Route>
 
       <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
+        <Movie setMovieList={setMovieList} addToSavedList={addToSavedList} />
       </Route>
       <Route path="/update-movie/:id">
         <UpdateMovie movies={movieList} />
--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from 'axios';
 
 //initial state to clear form
@@ -10,7 +10,7 @@ const FormState = {
   stars: []
 }
 
-export const MovieForm = ({ movie}) => {
+export const MovieForm = ({ movie }) => {
   const { push } = useHistory();
  // console.log('MovieForm',movie )
  //create state object using useState
